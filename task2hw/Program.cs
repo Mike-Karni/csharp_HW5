28b4b6af326eff0dd7a95f4dd823a648286aec52
mike_karni <3923499@mail.ru>
2022-04-26 22:03:33 +0300

    task2_done

--- task2hw/Program.cs
+++ task2hw/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
+
+Console.WriteLine("Введите длину массива ");
+int Length = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[Length];
+FillArray(numbers);
+PrintArray(numbers);
+int sum = 0;
+for (int i=0;i<numbers.Length;i++)
+{
+    int ostatok = i%2;
+    if(ostatok==1)
+    {
+        sum = sum + numbers[i];
+    }
+}
+Console.WriteLine("Cумма элементов, стоящих на нечётных позициях = " + sum);
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i<array.Length;i++)
+    {
+        array[i] = new Random().Next(1,100);
+    }
+}
+void PrintArray(int[] array1)
+{
+    for(int i = 0; i<array1.Length;i++)
+    {
+        Console.Write(array1[i]+ " ");
+       
+    }
+    Console.WriteLine();
+}
