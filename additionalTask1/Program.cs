fa3d9249b61ae7e52092efe113a8a9a891fca75a
mike_karni <3923499@mail.ru>
2022-04-29 07:32:37 +0300

    additionalTaskDone

---
+++ additionalTask1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается
+
+//if ввод не == "exit", то снова запрос ввода.
+
+//Console.WriteLine("Введите слово для выхода из цикла");
+//string password = Console.ReadLine();
+string password = "";
+while (password != "exit")
+{
+    Console.WriteLine("Введите слово для выхода из цикла");
+    password = Console.ReadLine();     
+    /*if (newpassword == "exit")
+    {
+        break;
+    }*/
+}
+
+/* ЧЕСТНО СПИЗДИЛ С ИНЕТА
+while (true) //вечный цикл
+{
+    Console.WriteLine("пароль для выхода:");
+    string input = Console.ReadLine(); //Получаем введенные данные 
+    if (input == "exit") //Если пользователь ввел слово exit, то 
+        break; //Выходим из вечного цикла
+    
+}*/
